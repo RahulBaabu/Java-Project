ffa871ff242bd9cf5796b47c848f714215c203f1
RahulBaabu <98244921+RahulBaabu@users.noreply.github.com>
2024-09-01 22:37:35 +0530

    Add files via upload

---
+++ src/jspinner1.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JSpinner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+class jspinner1 {
+
+    static void setEditor(JSpinner.DateEditor de) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
